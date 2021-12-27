d9aa01a7248d44fafe127434209d26ebc0f042d8
kianak2002 <61980014+kianak2002@users.noreply.github.com>
2021-12-27 19:01:12 +0330

    cp and cv added

--- controller.py
+++ controller.py
@@ -113,27 +113,109 @@ class FuzzyController:
             ccw_fast=ccw_fast
         )
 
-    def rules(self, input_pa, input_pv):
-        force_stop = []
-        force_left_fast = []
-        force_left_slow = []
-        force_right_fast = []
-        force_right_slow = []
-        y_up_more_right = input_pa.get('y_up_more_right')
-        y_up_right = input_pa.get('y_up_right')
-        y_up = input_pa.get('y_up')
-        y_up_left = input_pa.get('y_up_left')
-        y_up_more_left = input_pa.get('y_up_more_left')
-        y_down_more_left = input_pa.get('y_down_more_left')
-        y_down_left = input_pa.get('y_down_left')
-        y_down = input_pa.get('y_down')
-        y_down_right = input_pa.get('y_down_right')
-        y_down_more_right = input_pa.get('y_down_more_right')
-        cw_fast = input_pv.get('cw_fast')
-        cw_slow = input_pv.get('cw_slow')
-        stop = input_pv.get('stop')
-        ccw_slow = input_pv.get('ccw_slow')
-        ccw_fast = input_pv.get('ccw_fast')
+    def cp_fuzzy(self, cp):
+        left_far = 0
+        left_near = 0
+        stop = 0
+        right_near = 0
+        right_far = 0
+        if cp < -10:
+            left_far = 1
+        if cp > 10:
+            right_far = 1
+        if -10 <= cp <= -5:
+            left_far = -0.2 * cp - 1
+            left_near = (1 / 7.5) * cp + (10 / 7.5)
+        if -5 <= cp <= -2.5:
+            left_near = (1 / 7.5) * cp + (10 / 7.5)
+        if -2.5 <= cp <= 0:
+            left_near = (-1 / 2.5) * cp
+            stop = (1 / 2.5) * cp + 1
+        if 0 <= cp <= 2.5:
+            stop = (-1 / 2.5) * cp + 1
+            right_near = (1 / 2.5) * cp
+        if 2.5 <= cp <= 5:
+            right_near = (-1 / 7.5) * cp + (10 / 7.5)
+        if 5 <= cp <= 10:
+            right_far = 0.2 * cp - 1
+
+        return dict(
+            left_far=left_far,
+            left_near=left_near,
+            stop=stop,
+            right_near=right_near,
+            right_far=right_far
+        )
+
+    def cv_fuzzy(self, cv):
+        left_fast = 0
+        left_slow = 0
+        stop = 0
+        right_slow = 0
+        right_fast = 0
+
+        if cv < -5:
+            left_fast = 1
+        if cv > 5:
+            right_fast = 1
+        if -5 <= cv <= -2.5:
+            left_fast = (-1 / 2.5) * cv - 1
+            left_slow = 0.25 * cv + 1.25
+        if -2.5 <= cv <= -1:
+            left_slow = 0.25 * cv + 1.25
+        if -1 <= cv <= 0:
+            left_slow = -1 * cv
+            stop = cv + 1
+        if 0 <= cv <= 1:
+            stop = -1 * cv + 1
+            right_slow = cv
+        if 1 <= cv <= 2.5:
+            right_slow = -0.25 * cv + 1.25
+        if 2.5 <= cv <= 5:
+            right_slow = -0.25 * cv + 1.25
+            right_fast = (1 / 2.5) * cv - 1
+
+        return dict(
+            left_fast=left_fast,
+            left_slow=left_slow,
+            stop=stop,
+            right_slow=right_slow,
+            right_fast=right_fast
+        )
+
+
+
+    def rules(self, input_pa, input_pv, input_cp, input_cv):
+        force_stop = []  # force
+        force_left_fast = []  # force
+        force_left_slow = []  # force
+        force_right_fast = []  # force
+        force_right_slow = []  # force
+        y_up_more_right = input_pa.get('y_up_more_right')  # pa
+        y_up_right = input_pa.get('y_up_right')  # pa
+        y_up = input_pa.get('y_up')  # pa
+        y_up_left = input_pa.get('y_up_left')  # pa
+        y_up_more_left = input_pa.get('y_up_more_left')  # pa
+        y_down_more_left = input_pa.get('y_down_more_left')  # pa
+        y_down_left = input_pa.get('y_down_left')  # pa
+        y_down = input_pa.get('y_down')  # pa
+        y_down_right = input_pa.get('y_down_right')  # pa
+        y_down_more_right = input_pa.get('y_down_more_right')  # pa
+        cw_fast = input_pv.get('cw_fast')  # pv
+        cw_slow = input_pv.get('cw_slow')  # pv
+        stop = input_pv.get('stop')  # pv
+        ccw_slow = input_pv.get('ccw_slow')  # pv
+        ccw_fast = input_pv.get('ccw_fast')  # pv
+        left_far = input_cp.get('left_far')  # cp
+        left_near = input_cp.get('left_near')  # cp
+        cp_stop = input_cp.get('stop')  # cp
+        right_near = input_cp.get('right_near')  # cp
+        right_far = input_cp.get('right_far')  # cp
+        left_fast = input_cv.get('left_fast')  # cv
+        left_slow = input_cv.get('left_slow')  # cv
+        cv_stop = input_cv.get('stop')  # cv
+        right_slow = input_cv.get('right_slow')  # cv
+        right_fast = input_cv.get('right_fast')  # cv
 
         #  rules
         force_stop.append(max(min(y_up, stop), min(y_up_right, ccw_slow), min(y_up_left, cw_slow)))
@@ -148,8 +230,8 @@ class FuzzyController:
         force_right_fast.append(min(y_down_more_right, ccw_slow))
         force_stop.append(min(y_down_more_right, cw_slow))
         force_left_fast.append(min(y_down_more_left, cw_slow))
-        force_stop.append(min(y_down_more_left, ccw_slow))
-        force_stop.append(min(y_down_more_right, ccw_fast))
+        force_stop.append(min(y_down_more_left, ccw_slow, cp_stop))  ########cp
+        force_stop.append(min(y_down_more_right, ccw_fast, cp_stop))  #########cp
         force_stop.append(min(y_down_more_right, cw_fast))
         force_stop.append(min(y_down_more_left, cw_fast))
         force_stop.append(min(y_down_more_left, ccw_fast))
@@ -163,21 +245,21 @@ class FuzzyController:
         force_left_slow.append(min(y_down_left, ccw_fast))
         force_right_slow.append(min(y_up_right, ccw_slow))
         force_right_fast.append(min(y_up_right, stop))
-        force_left_slow.append(min(y_up_left, cw_slow))
-        force_left_fast.append(min(y_up_left, ccw_slow))
-        force_left_fast.append(min(y_up_left, stop))
-        force_left_fast.append(min(y_up_right, ccw_fast))
-        force_right_fast.append(min(y_up_right, cw_fast))
+        force_left_slow.append(min(y_up_left, cw_slow, left_slow)) ######cv
+        force_left_fast.append(min(y_up_left, ccw_slow, left_fast)) #######cv
+        force_left_fast.append(min(y_up_left, stop, cv_stop)) #######cv
+        force_left_fast.append(min(y_up_right, ccw_fast, right_fast)) ########cv
+        force_right_fast.append(min(y_up_right, cw_fast, right_slow))  ########cv
         force_right_fast.append(min(y_up_left, cw_fast))
         force_left_fast.append(min(y_up_left, ccw_fast))
         force_right_fast.append(min(y_down, stop))
-        force_stop.append(min(y_down, cw_fast))
+        force_stop.append(min(y_down, cw_fast, cp_stop))  ########cp
         force_stop.append(min(y_down, ccw_fast))
         force_left_slow.append(min(y_up, ccw_slow))
-        force_left_fast.append(min(y_up, ccw_fast))
+        force_left_fast.append(min(y_up, ccw_fast, ))
         force_right_slow.append(min(y_up, cw_slow))
-        force_right_fast.append(min(y_up, cw_fast))
-        force_stop.append(min(y_up, stop))
+        force_right_fast.append(min(y_up, cw_fast, left_near))  ##########cp
+        force_stop.append(min(y_up, stop, cp_stop))  ########cp
 
         force_stop_max = max(force_stop)
         force_left_fast_max = max(force_left_fast)
@@ -240,9 +322,9 @@ class FuzzyController:
                 if x_left_slow_1 < x < x_left_slow_2:
                     y1 = left_slow
                 elif x_left_slow_2 < x:
-                    y1 = -(1.0/60) * x
+                    y1 = -(1.0 / 60) * x
                 if x < x_stop_1:
-                    y2 = (1.0/60) * x + 1
+                    y2 = (1.0 / 60) * x + 1
                 elif x_stop_1 < x:
                     y2 = stop
                 y = max(y1, y2)
@@ -283,14 +365,13 @@ class FuzzyController:
             return 0
         return sum_nominator / sum_denominator
 
-
-
-
     def decide(self, world):
         input = self._make_input(world)
         input_pa = self.pa_fuzzy(input.get('pa'))
         input_pv = self.pv_fuzzy(input.get('pv'))
-        force = self.rules(input_pa, input_pv)
+        input_cp = self.cp_fuzzy(input.get('cp'))
+        input_cv = self.cv_fuzzy(input.get('cv'))
+        force = self.rules(input_pa, input_pv, input_cp, input_cv)
         defuzzified_force = self.defuzzify_force(force)
         # output = self._make_output()
         # self.system.calculate(self._make_input(world), output)
